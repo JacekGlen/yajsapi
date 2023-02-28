2c04bf488623d6f06923dbc28a9617ba98e3ab7d
Marcin Gordel <marcin.gordel@gmail.com>
2023-02-28 09:33:20 +0100

    Example draft

---
+++ examples/reality/index.ts
@@ -0,0 +1,20 @@
+import { TaskExecutor } from "../..";
+import { program } from "commander";
+
+async function main(timeout: number, threshold: number) {
+  const executor = await TaskExecutor.create("9a3b5d67b0b27746283cb5f287c13eab1beaa12d92a9f536b747c7ae");
+  const results = await executor.run(async (ctx) => {
+    const res1 = await ctx.run('echo "Hello"');
+    const res2 = await ctx.run('echo "World"');
+    return `${res1.stdout}${res2.stdout}`;
+  });
+  console.log(results);
+  await executor.end();
+}
+
+program
+  .requiredOption("--timeout <timeout>", "time period of collecting offer")
+  .requiredOption("--threshold <threshold>", "minimal cost");
+program.parse();
+const options = program.opts();
+main(options.timeout, options.threshold);
